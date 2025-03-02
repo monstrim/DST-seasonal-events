f324fb26a8290954993b3f74fd3c3d233f8af342
Pedro de Castro Araujo <pedroaraujo@natura.net>
2025-03-02 00:53:26 -0300

    Fix (?) for lunacy overlay trigger - event hash onflict? - removed debug prints

--- modmain.lua
+++ modmain.lua
@@ -12,11 +12,11 @@ AddPrefabPostInit('cave_network', function (inst) if GLOBAL.TheWorld.ismastersim
 -- route flareoverlay and batspooked events from server to client
 AddPlayerPostInit(function (player) player:DoTaskInTime(0, 
     function(player)
-        _spook_trigger = GLOBAL.net_event(player.GUID, 'seasonalevents._spook_trigger')
-        _flare_trigger = GLOBAL.net_event(player.GUID, 'seasonalevents._flare_trigger')
-        _flare_r = GLOBAL.net_float(player.GUID, 'seasonalevents._flare_r')
-        _flare_g = GLOBAL.net_float(player.GUID, 'seasonalevents._flare_g')
-        _flare_b = GLOBAL.net_float(player.GUID, 'seasonalevents._flare_b')
+        _spook_trigger = GLOBAL.net_event(player.GUID, 'seasonalevents._spooktrigger')
+        _flare_trigger = GLOBAL.net_event(player.GUID, 'seasonalevents._flaretrigger')
+        _flare_r = GLOBAL.net_float(player.GUID, 'seasonalevents._flarer')
+        _flare_g = GLOBAL.net_float(player.GUID, 'seasonalevents._flareg')
+        _flare_b = GLOBAL.net_float(player.GUID, 'seasonalevents._flareb')
 
         if player == GLOBAL.ThePlayer then
             -- setup listening on client
@@ -27,7 +27,7 @@ AddPlayerPostInit(function (player) player:DoTaskInTime(0,
                 hud.batover = hud.overlayroot:AddChild(BatOver(player))
             end
 
-            player:ListenForEvent('seasonalevents._flare_trigger', function(player) 
+            player:ListenForEvent('seasonalevents._flaretrigger', function(player) 
                 data = {
                     r = _flare_r:value(),
                     g = _flare_g:value(),
@@ -36,7 +36,7 @@ AddPlayerPostInit(function (player) player:DoTaskInTime(0,
                 player:PushEvent('startflareoverlay', data)
             end)
 
-            player:ListenForEvent('seasonalevents._spook_trigger', function(player) 
+            player:ListenForEvent('seasonalevents._spooktrigger', function(player) 
                 player:PushEvent('batspooked')
             end)
         else
