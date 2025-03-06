df3fad0583d1a4723a3f152fd24e02d63f291f01
Pedro de Castro Araujo <pedroaraujo@natura.net>
2025-03-06 11:36:49 -0300

    added carnival fanfarre

--- scripts/ui_utils.lua
+++ scripts/ui_utils.lua
@@ -16,7 +16,6 @@ function _announce(event)
 end
 
 function _playSound(sound, name, volume)
-    --TODO: test difference between TheWorld and ThePlayer in multiplayer
     for i,v in ipairs(AllPlayers) do v.SoundEmitter:PlaySound(sound, name, volume) end
 end
 
@@ -49,6 +48,27 @@ function _hallowednightstorm()
     TheWorld:DoTaskInTime(13, function() SpawnPrefab('thunder_far') end)
 
     for i,v in ipairs(AllPlayers) do v:PushEvent('batspooked') end
+end
+
+-------------------
+
+function _carnivalconfetti()
+    for _, player in ipairs(AllPlayers) do
+        for i=1,10 do
+            player:DoTaskInTime(math.random() * 4, function(player) 
+                local angle = math.random() * 2 * math.pi
+                local r = math.random() * 10
+                local x,y,z = player.Transform:GetWorldPosition()
+                local dx = math.sin(angle) * r
+                local dz = math.cos(angle) * r
+                SpawnPrefab('carnival_confetti_fx').Transform:SetPosition(x+dx, y, z+dz) 
+            end)
+        end
+    end
+
+    _playSound('summerevent2022/carnivalgame_wheelspin/turn_on')
+    
+    TheWorld:PushEvent('ms_forceprecipitation', false)
 end
 
 -------------------
