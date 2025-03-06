6ffcc385dcc5334b0c29dacff92d88e31a5a7091
Pedro de Castro Araujo <pedroaraujo@natura.net>
2025-03-06 11:52:17 -0300

    1.2

--- modinfo.lua
+++ modinfo.lua
@@ -2,20 +2,20 @@
 
 name = "Yearly Seasonal Events"
 description = [[
-Version 1.1.1
+Version 1.2
 
 󰀭 There's always one Year Of event active, which cycles on the second new moon after beginning of winter.
 
 󰀧 Special Events (Hallowed Eve, Summer Cawnival and Winter's Feast) happen every in-game year.
 
 ----------------------------------------
-󰀏 Fixed for caves and multiplayer! Also, won't explode again before I patch it the next time a Year Of event is launched.
-󰀓 Is the Lunacy UI bug gone? Is it still around? We may never know! (hoping I fixed it)
-󰀛 Minor fixes for seasonal events lenghts, specially for short seasons (no rainy christmas ever)
+󰀏 Added confetti fx fanfarre to Carnival start.
+󰀓 Localized strings for event announcements!
+󰀛 (More) minor fixes for seasonal events lenghts.
 ]]
 
 author = "Monstah"
-version = "1.1.2"
+version = "1.2"
 
 
 api_version = 10
