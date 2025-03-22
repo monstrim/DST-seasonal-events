964d7f0e98ad02768db33aa4f8f2e8dfff7dc2d6
Pedro de Castro Araujo <pedroaraujo@natura.net>
2025-03-21 21:09:09 -0300

    1.3.1

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@
 
 name = "Yearly Seasonal Events"
 description = [[
-Version 1.3
+Version 1.3.1
 
 󰀧 Special Events (Hallowed Eve, Summer Cawnival and Winter's Feast) happen every in-game year.
 
@@ -16,7 +16,7 @@ Version 1.3
 ]]
 
 author = "Monstah"
-version = "1.3"
+version = "1.3.1"
 
 
 api_version = 10
