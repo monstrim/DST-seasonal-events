026ea91d6e3cd75cdfc9885be1c59feb433e431d
Pedro de Castro Araujo <pedroaraujo@natura.net>
2025-03-26 00:14:49 -0300

    refactored fireworks

--- scripts/utils/ui_utils.lua
+++ scripts/utils/ui_utils.lua
@@ -62,17 +62,19 @@ end
 -------------------
 
 function _carnivalconfetti()
+    local function _throw_fx(player) 
+        local angle = math.random() * 2 * math.pi
+        local r = math.random() * 10
+        local x,y,z = player.Transform:GetWorldPosition()
+        local dx = math.sin(angle) * r
+        local dz = math.cos(angle) * r
+        SpawnPrefab('carnival_confetti_fx').Transform:SetPosition(x+dx, y, z+dz) 
+    end
+
     if TheWorld.ismastersim then
         for _, player in ipairs(AllPlayers) do
-            for i=1,10 do
-                TheWorld:DoTaskInTime(math.random() * 4, function() 
-                    local angle = math.random() * 2 * math.pi
-                    local r = math.random() * 10
-                    local x,y,z = player.Transform:GetWorldPosition()
-                    local dx = math.sin(angle) * r
-                    local dz = math.cos(angle) * r
-                    SpawnPrefab('carnival_confetti_fx').Transform:SetPosition(x+dx, y, z+dz) 
-                end)
+            if not player:HasTag('ghost') then 
+                for i=1,10 do player:DoTaskInTime(math.random() * 4, _throw_fx) end
             end
         end
 
